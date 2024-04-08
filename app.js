d3d5c9180e073190bd2240e32017b0debdd6bf68
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-09 00:02:16 +0630

    event-handling, v-bind, v-for, computed property

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: "you don't know js",
-            author: "ktz",
-            age: 20,
-            showBook: true
+            books: [
+                {
+                    title: "You don't know JS",
+                    author: "KTZ",
+                    age: 27,
+                    img: "imgs/img1.jpg",
+                    isFav: true
+                },
+                {
+                    title: "Book 2",
+                    author: "AA",
+                    age: 28,
+                    img: "imgs/img2.jpg",
+                    isFav: false
+                },
+                {
+                    title: "Book 3",
+                    author: "BB",
+                    age: 29,
+                    img: "imgs/img3.jpg",
+                    isFav: true
+                }
+            ],
+            showBook: true,
+            link: "http://www.google.com",
         }
     },
     methods: {
-        increaseAge(){
-            this.age++;
+        mouseoverHandler(event, data) {
+            console.log('mouseover is working', event.type, data);
+        },
+        mouseleaveHandler(event) {
+            console.log('mouseleave is working', event.type);
+        },
+        dblclickHandler(event) {
+            console.log('dbl click', event.type);
+        },
+        clickHandler(book) {
+            book.isFav = !book.isFav;
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter(book => {
+                return book.isFav;
+            });
         }
     }
 });
